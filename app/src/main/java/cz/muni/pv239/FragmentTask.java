7116361950b15597e854153fb1979cb9c1eabfd0
xtrnkal <trnkalovaa@seznam.cz>
2019-04-28 09:28:20 +0200

    changing fragment layout - not working

--- app/src/main/java/cz/muni/pv239/FragmentTask.java
+++ app/src/main/java/cz/muni/pv239/FragmentTask.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class FragmentTask extends Fragment {
+public class FragmentTask extends Fragment implements View.OnClickListener {
     View view;
+
+
     Button openTimerButton = null;
 
     public FragmentTask() {
@@ -22,36 +25,32 @@ public class FragmentTask extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.task_fragment, container, false);
 
+
         openTimerButton = (Button) view.findViewById(R.id.brain1);
-        openTimerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(view.getContext(), FragmentTime.class);
-                startActivity(intent);
-            }
-        });
+        openTimerButton.setOnClickListener(this);
+
         openTimerButton = (Button) view.findViewById(R.id.brain2);
-        openTimerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(view.getContext(), FragmentTime.class);
-                startActivity(intent);
-            }
-        });
+        openTimerButton.setOnClickListener(this);
         openTimerButton = (Button) view.findViewById(R.id.brain3);
-        openTimerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(view.getContext(), FragmentTime.class);
-                startActivity(intent);
-            }
-        });
+        openTimerButton.setOnClickListener(this);
 
         return view;
     }
 
-    private void openTimer(){
 
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.brain1:
+
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.task_fragment_container, new FragmentTime());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+                break;
+        }
     }
 }
